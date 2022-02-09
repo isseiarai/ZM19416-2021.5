b6ed9aeb2abc73da9497e0ea953adb23bb8703e7
isseiarai <zm19416@josai.ac.jp>
2022-02-09 03:39:05 +0000

    02/09

---
+++ ex1.js
@@ -0,0 +1,17 @@
+function sayhello1(){
+    alert('Hello,'+ document.getElementById('who1').value+'!');
+}
+
+    function img() {document.getElementById('logo1').src ="./image/kadai2.png";}
+    
+function imgchange4() {document.getElementById('logo1').src='./image/kadai3.png';}
+function bthrees() {document.getElementById('logo1').src='./image/kadai1.png';}
+
+function nazo(){
+    if(document.getElementById('answer').value=='ガブリアス'){
+    alert('当たり');
+    }
+      else{
+        alert('不正解！');
+    }
+    }
